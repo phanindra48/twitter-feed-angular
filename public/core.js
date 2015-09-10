43b887bbf56ce740e36bfea772afc0e04a3e98a0
Phanindra Pydisetty <phanindra.nitw@gmail.com>
2015-09-10 13:50:54 +0530

    Loading feed based on user location

--- public/core.js
+++ public/core.js
@@ -7,47 +7,22 @@ twitterFeedApp.controller('mainController', ['$scope', function($scope) {
 
 }]);
 twitterFeedApp.controller('feedController', ['$scope', '$http', function($scope, $http) {
-    // $scope.latitude = '';
-    // $scope.longitude = '';
-    // var getLocation = function() {
-    //     if (navigator.geolocation) {
-    //         navigator.geolocation.getCurrentPosition(showPosition);
-    //     } else {
-    //         x.innerHTML = "Geolocation is not supported by this browser.";
-    //     }
-    // };
-
-    // var showPosition = function(position) {
-    //     //alert('show position');
-    //     $scope.latitude = position.coords.latitude;
-    //     $scope.longitude = position.coords.longitude;
-    //     console.log('sp latitude: ', $scope.latitude);
-    //     console.log('sp longitude: ', $scope.longitude);
-    // };
-    // $scope.init = function() {
-    //     //alert('sfsdfsdfg');
-    //     getLocation();
-    //     //console.log('latitude: ', $scope.latitude);
-    //     //console.log('longitude: ', $scope.longitude);
-    //     //$scope.getfeed();
-    // };
-
-
-
-
-    $scope.latitude = "0";
-    $scope.longitude = "0";
+    $scope.latitude = '0';
+    $scope.longitude = '0';
     $scope.accuracy = "0";
+    $scope.formData = {};
     $scope.error = "";
-
-    $scope.showPosition = function(position) {
-        $scope.latitude = position.coords.latitude;
-        $scope.longitude = position.coords.longitude;
-        $scope.accuracy = position.coords.accuracy;
-        $scope.$apply();
-    }
-
-    $scope.showError = function(error) {
+    var getLocation = function() {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(showPosition, showError);
+        } else {
+            x.innerHTML = "Geolocation is not supported by this browser.";
+        }
+    };
+    var showResult = function() {
+        return $scope.error == "";
+    };
+    var showError = function(error) {
         switch (error.code) {
             case error.PERMISSION_DENIED:
                 $scope.error = "User denied the request for Geolocation."
@@ -63,31 +38,25 @@ twitterFeedApp.controller('feedController', ['$scope', '$http', function($scope,
                 break;
         }
         $scope.$apply();
-    }
-
-    $scope.getLocation = function() {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition($scope.showPosition, $scope.showError);
-        } else {
-            $scope.error = "Geolocation is not supported by this browser.";
-        }
-    }
-
-    $scope.getLocation();
-
-    $scope.formData = {
-        latitude: $scope.latitude,
-        longitude: $scope.longitude
     };
-    console.log($scope.formData);
-    $http.post('/api/feed', $scope.formData)
-        .success(function(data) {
-            console.log(data);
-            $scope.tweets = data.statuses;
-        })
-        .error(function(err) {
-            console.log('Error: ' + err);
-        });
+    var showPosition = function(position) {
+        //alert('show position');
+        $scope.latitude = position.coords.latitude;
+        $scope.longitude = position.coords.longitude;
+        $scope.accuracy = position.coords.accuracy;
+        //console.log('sp latitude: ', $scope.latitude);
+        //console.log('sp longitude: ', $scope.longitude);
+        $scope.$apply();
+        $scope.formData = {
+            latitude: $scope.latitude,
+            longitude: $scope.longitude
+        };
+        $scope.getfeed();
+    };
+    $scope.init = function() {
+        getLocation();
+    };
+
     $scope.getfeed = function() {
 
         //console.log($scope.formData);
